3edc01bc8819728b0ed5748202ed0b71e7262635
Imad-Dabagh <imaddabagh@gmail.com>
2025-03-10 19:59:46 +0000

    Completed exercise 8

--- week_1/day_1/exercise_8.py
+++ week_1/day_1/exercise_8.py
@@ -1,5 +1,29 @@
+sandwich_orders = ["Tuna sandwich", "Pastrami sandwich", "Avocado sandwich", "Pastrami sandwich", "Egg sandwich", "Chicken sandwich", "Pastrami sandwich"]
 
+# Use a while loop to remove all occurrences of Pastrami sandwich from the list
 
+while "Pastrami sandwich" in sandwich_orders:
+    sandwich_orders.remove("Pastrami sandwich")
 
+#print(sandwich_orders)
 
+# Create an empty list called finished_sandwiches
+finished_sandwiches = []
 
+# Remove each sandwich from 'sandwich_orders' and add them to 'finished_sandwiches'
+
+while sandwich_orders:
+    sandwiche = sandwich_orders.pop(0)
+    finished_sandwiches.append(sandwiche)
+
+print(finished_sandwiches)
+
+# After all the sandwiches have been made, print a message listing each sandwich that was made
+
+# The complex way
+for sandwich in range(0, len(finished_sandwiches)):
+    print(f"I made your {finished_sandwiches[sandwich]}")
+
+# A simpler way
+    # for sandwich in finished_sandwiches:
+    #     print(f"I made your {sandwich}")
