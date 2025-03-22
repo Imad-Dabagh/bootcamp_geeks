9570ec575bbce62493d0949586537bfc67e3d0d3
Imad-Dabagh <imaddabagh@gmail.com>
2025-03-22 10:15:28 +0000

    Added some features to API: Price of an item | AVG Price of items

--- week_2/day_4/exercise/menu_manager.py
+++ week_2/day_4/exercise/menu_manager.py
@@ -1,38 +1,6 @@
 from connection import DbConnection
 
 
-# class MenuManager:
-
-    # @classmethod
-    # def get_by_name(cls, item_name):
-    #     db = DbConnection()
-    #     connection = db.connect()
-    #     cursor = connection.cursor()
-
-    #     cursor.execute(
-    #         "SELECT * FROM Menu_Items WHERE item_name = %s", (item_name,))
-    #     item = cursor.fetchone()
-
-    #     cursor.close()
-    #     connection.close()
-
-    #     return item
-    
-    # @classmethod
-    # def all_items(cls):
-    #     db = DbConnection()
-    #     connection = db.connect()
-    #     cursor = connection.cursor()
-
-    #     cursor.execute(
-    #         "SELECT * FROM Menu_Items")
-    #     items = cursor.fetchall()
-
-    #     cursor.close()
-    #     connection.close()
-
-    #     return items
-
 class MenuManager:
     @classmethod
     def get_by_name(cls, item_name):
