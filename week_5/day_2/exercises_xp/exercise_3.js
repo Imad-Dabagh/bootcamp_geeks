59e4db28d329b0e0a6cc6d8cdc72f67ff5cc9965
Imad-Dabagh <imaddabagh@gmail.com>
2025-04-09 11:07:33 +0100

    Week_5 Day_2 Completed Exercises XP

--- week_5/day_2/exercises_xp/exercise_3.js
+++ week_5/day_2/exercises_xp/exercise_3.js
@@ -4,6 +4,7 @@
 // =====    .then(response => response.json())
 // =====    .then(objectStarWars => console.log(objectStarWars.result));
 
+
 const asyncFucntion = async () => {
     try {
         const response = await fetch("https://www.swapi.tech/api/starships/9/")
